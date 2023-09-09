a2059805f048ca66d0ff6af20fceb7ccd7093473
shodai yamatsu <>
2023-09-09 13:35:47 +0900

    change

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <form class="contact_form" name="contact" method="POST" data-netlify="true" netlify>
-            <input type="hidden" name="form-name" :value="contact" />
+            <input type="hidden" name="form-name" value="contact" />
             <div class="contact_item">
                 <label for="name">お名前</label>
                 <input type="text" id="name" name="name" autocomplete="name" />
