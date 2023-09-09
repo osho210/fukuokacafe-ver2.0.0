385fa80b8ccbc437564c643805201011987f129b
shodai yamatsu <>
2023-09-09 12:36:46 +0900

    Create a contact page

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,13 +1,20 @@
 <template>
-    <form name="contact" netlify>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
-    </form>
+    <div class="container">
+        <h1>お問い合わせ</h1>
+        <form name="contact" method="POST" data-netlify="true">
+            <div>
+                <label for="name">お名前（必須）</label>
+                <input name="name" type="text" required />
+            </div>
+            <div>
+                <label for="email">メールアドレス（必須）</label>
+                <input name="email" type="email" required />
+            </div>
+            <div>
+                <label for="content">お問い合わせ内容</label>
+                <textarea name="content" rows="10" required></textarea>
+            </div>
+            <button type="submit">送信する</button>
+        </form>
+    </div>
 </template>
