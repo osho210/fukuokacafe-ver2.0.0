3bf97300aa079c7aedafaf569f5846d34dfe6d7a
shodai yamatsu <>
2023-09-09 13:06:39 +0900

    prayer

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,21 +1,56 @@
 <template>
     <div class="container">
-        <h1>お問い合わせ</h1>
-        <form name="contact" method="POST" data-netlify="true">
+        <form class="contact_form" name="contact" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
-            <div>
-                <label for="name">お名前（必須）</label>
-                <input name="name" type="text" required />
+            <div class="contact_item">
+                <label for="name">お名前</label>
+                <input type="text" id="name" name="name" autocomplete="name" />
             </div>
-            <div>
-                <label for="email">メールアドレス（必須）</label>
-                <input name="email" type="email" required />
+            <div class="contact_item">
+                <label for="email">メールアドレス</label>
+                <input type="email" id="email" name="email" autocomplete="email" />
             </div>
-            <div>
-                <label for="content">お問い合わせ内容</label>
-                <textarea name="content" rows="10" required></textarea>
+            <div class="contact_item">
+                <label for="message">お問い合わせ内容</label>
+                <textarea id="message" rows="12" name="message"></textarea>
             </div>
-            <button type="submit">送信する</button>
+            <div class="contact_submit">
+                <button type="submit">送信</button>
+            </div>
         </form>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.contact_form .contact_item {
+    display: flex;
+    margin-bottom: 2%;
+}
+
+.contact_form .contact_item label {
+    width: 20%;
+}
+
+.contact_form .contact_item input,
+.contact_form .contact_item textarea {
+    width: 80%;
+}
+
+.contact_form .contact_submit {
+    text-align: center;
+}
+</style>
