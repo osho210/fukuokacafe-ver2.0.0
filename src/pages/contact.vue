bbe354b36e58c1ee29c515d418d9b4b6cd4c3a65
shodai yamatsu <>
2023-09-10 02:21:07 +0900

    Change Contact file

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,14 +1,14 @@
 <template>
-    <form name="contact" data-netlify="true" netlify>
+    <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact">
         <p>
-            <label>Name: <input type="text" name="name" /></label>
+            <label>Your Name: <input type="text" name="name" /></label>
         </p>
         <p>
             <label>Your Email: <input type="email" name="email" /></label>
         </p>
         <p>
-            <label>Role: <select name="role[]" multiple>
+            <label>Your Role: <select name="role[]" multiple>
                     <option value="leader">Leader</option>
                     <option value="follower">Follower</option>
                 </select></label>
@@ -21,24 +21,3 @@
         </p>
     </form>
 </template>
-
-<script setup>
-const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-    })
-        .then(() => console.log("Form successfully submitted"))
-        .catch((error) => alert(error));
-};
-
-document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-</script>
