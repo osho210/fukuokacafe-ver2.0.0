5132c081c0e3d7705f87a85be9fc19810f223478
shodai yamatsu <>
2023-09-09 22:27:15 +0900

    Add handleSubmit function

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -21,3 +21,24 @@
         </p>
     </form>
 </template>
+
+<script setup>
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
+};
+
+document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+</script>
