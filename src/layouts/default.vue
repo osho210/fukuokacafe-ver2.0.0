39b0d68dab54cf822417815411803cbd9a4f1aa7
shodai yamatsu <>
2023-09-11 10:24:38 +0900

    environment variable

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -20,7 +20,7 @@
 import { createClient } from '@supabase/supabase-js'
 const $config = useRuntimeConfig()
 const supabaseUrl = $config.public.VITE_SUPABASE_URL
-console.log(supabaseUrl+"supabaseUrl")
+console.log(supabaseUrl+"supabaseUrlx")
 const supabaseApiKey = $config.public.VITE_SUPABASE_API_KEY
 const supabase = createClient(supabaseUrl, supabaseApiKey)
 const countries = ref<any[]>([]);
