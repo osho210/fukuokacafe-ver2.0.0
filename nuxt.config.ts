fdb12d3dbda106bbb1c61095214010e523dffa52
shodai yamatsu <>
2023-09-13 00:01:00 +0900

    Added to HEAD because it does not read

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,20 @@ export default defineNuxtConfig({
   plugins: ['~/plugins/globalVariables.ts'],
   modules: ['nuxt-swiper','nuxt-clarity-analytics'],
   ssr:false,
+  app:{
+    head: {
+      "script": [
+          {
+            innerHTML: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "iu1fzbkjnf");`,
+            type: 'text/javascript',
+          },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
