c93db2ec31c22fa4611b8bbf1e72cd3422faceb6
shodai yamatsu <>
2023-09-12 23:41:40 +0900

    Implement Clarity analytics integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,13 @@ export default defineNuxtConfig({
   srcDir: 'src/',
   css: ['~/assets/css/reset.css', '~/assets/css/common.css'],
   plugins: ['~/plugins/globalVariables.ts'],
-  modules: ['nuxt-swiper'],
+  modules: ['nuxt-swiper','nuxt-clarity-analytics'],
   ssr:false,
   runtimeConfig: {
     public: {
       VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
-      VITE_SUPABASE_API_KEY: process.env.VITE_SUPABASE_API_KEY
-    },
+      VITE_SUPABASE_API_KEY: process.env.VITE_SUPABASE_API_KEY,
+      VITE_MICROSOFT_CLARITY_ID: process.env.VITE_MICROSOFT_CLARITY_ID
+    }
   }
 })
